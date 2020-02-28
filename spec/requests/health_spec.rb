8059ed006c870481ed8cea15a20c3fa0d1ccd13e
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-02-28 00:39:06 -0300

    Add health request tests

---
+++ spec/requests/health_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Health', :type => :request do
+  let(:response) { get('health.json') }
+  let(:response_parsed_body) { JSON.parse(response.body) }
+
+  it 'have json content type' do
+    expect(response.content_type).to include('application/json')
+  end
+
+  it 'informs the correct environment' do
+    expect(response_parsed_body['env']).to eq(ENV['RACK_ENV'])
+  end
+end
