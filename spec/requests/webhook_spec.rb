103f8134cc0e60d978a35fc7e7eb5894cc16491e
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-03-29 18:07:23 -0300

    webhook request parser

--- spec/requests/webhook_spec.rb
+++ spec/requests/webhook_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Webhook', :type => :request do
-  let(:response) { post('webhook.json') }
+RSpec.describe '#webhook', :type => :request do
+  fixture('intents/ola_mundo.json', false)
+
+  let(:response) { post('webhook.json', ola_mundo) }
   let(:response_parsed_body) { JSON.parse(response.body) }
 
   it 'have json content type' do
