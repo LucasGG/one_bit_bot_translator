00ab5e7996b95566a884eb6241d3dc5fdc8ad589
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-03-30 18:49:54 -0300

    convert more languages

--- app/services/action_resolver/translate.rb
+++ app/services/action_resolver/translate.rb
@@ -3,14 +3,31 @@
 module ActionResolver
   # Call translations to another available service.
   class Translate < Service
+    CORRESPONDENCES = {
+      'português' => 'pt',
+      'inglês' => 'en',
+      'russo' => 'ru',
+      'japonês' => 'jp'
+    }.freeze
+
     def initialize(languages:, text:)
       @languages = languages
       @text = text
     end
 
     def call
-      # @parameters['languages'].first
-      Yandex::Translate.call(:text => @text, :ilang => 'pt', :olang => 'en')
+      first_lang, second_lang = @languages
+      ilang = language_code(second_lang ? first_lang : 'Português')
+      olang = language_code(second_lang || first_lang)
+      raise Error, 'languages not identified' unless ilang && olang
+
+      Yandex::Translate.call(:text => @text, :ilang => ilang, :olang => olang)
+    end
+
+    private
+
+    def language_code(lang)
+      CORRESPONDENCES[lang.downcase]
     end
   end
 end
